178cb31cf5600f0ca00fd93c3db82b5d6e85b49f
TheStrix <parthbhatia98@gmail.com>
2015-04-09 05:49:55 -0400

    AICP'd

--- aicp.mk
+++ aicp.mk
@@ -2,13 +2,13 @@
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
-# Inherit some common CM stuff
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common AICP stuff
+$(call inherit-product, vendor/aicp/configs/common.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/xiaomi/armani/full_armani.mk)
 
 PRODUCT_RELEASE_NAME := Redmi 1S
-PRODUCT_NAME := cm_armani
+PRODUCT_NAME := aicp_armani
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,1 +1,1 @@
-add_lunch_combo cm_armani-userdebug
+add_lunch_combo aicp_armani-userdebug
