cbc3ebeeb5dc3c604769dbbf00bc8c897df7b746
TheStrix <parthbhatia98@gmail.com>
2016-01-21 20:15:33 +0530

    armani: Fix missing updates from upstream
    - 'git commit --amend' it the culprit :p
    Change-Id: I7a9b24897f7bb842df69e2f50406180d5ca02751

--- device.mk
+++ device.mk
@@ -62,6 +62,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PACKAGES += \
     init.qcom.bt.sh
 
+# CABL
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.qualcomm.cabl=0
+
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
     camera2.portability.force_api=1
@@ -139,11 +143,7 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
 
 PRODUCT_PACKAGES += \
-    libOmxAacEnc \
-    libOmxAmrEnc \
     libOmxCore \
-    libOmxEvrcEnc \
-    libOmxQcelp13Enc \
     libOmxVdec \
     libOmxVenc \
     libstagefrighthw
@@ -175,10 +175,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     power.msm8226
 
-# Qualcomm
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.timed.enable=true \
-    ro.qualcomm.cabl=0 \
     ro.vendor.extension_library=/vendor/lib/libqti-perfd-client.so
 
 # Ramdisk
@@ -195,6 +192,10 @@ PRODUCT_COPY_FILES += \
 # Thermal
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine-8226.conf:system/etc/thermal-engine-8226.conf
+
+# Time services
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.timed.enable=true
 
 # Wifi
 PRODUCT_PACKAGES += \
