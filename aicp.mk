6b9475ee1ee870ecc3dba56da9d142a57038017d
Hemant Sharma <hemantbeast@gmail.com>
2018-01-14 21:22:31 +0530

    armani: add aicp.mk and update deps
    Change-Id: I6928a7f43364876bbf750b99cdd14da5a6f8d577

---
+++ aicp.mk
@@ -0,0 +1,34 @@
+# Inherit some common AICP stuff
+$(call inherit-product, vendor/aicp/config/common_full_phone.mk)
+
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from hardware-specific part of the product configuration
+$(call inherit-product, device/xiaomi/armani/device.mk)
+$(call inherit-product-if-exists, vendor/xiaomi/armani/armani-vendor.mk)
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_DEVICE := armani
+PRODUCT_NAME := aicp_armani
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := HM 1S
+PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+TARGET_VENDOR := xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=armani
+
+# AICP Device Maintainers
+PRODUCT_BUILD_PROP_OVERRIDES += \
+	DEVICE_MAINTAINERS="Hemant Sharma (hemantbeast)"
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
+-include vendor/aicp/configs/bootanimation.mk
+
+TARGET_OTA_ASSERT_DEVICE := armani
