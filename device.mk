fe31bc25422eebcc2822d3131ee05fa2a3352d0b
Hemant Sharma <hemantbeast@gmail.com>
2018-01-22 15:24:54 +0530

    armani: Remove additional property override
    Change-Id: I7a3433e1ea71dff674d1da44c4836172662fcb61

--- device.mk
+++ device.mk
@@ -40,9 +40,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.dex2oat-flags=--no-watch-dog \
     dalvik.vm.dex2oat-swap=false \
-    dalvik.vm.dex2oat-filter=speed
-
-ADDITIONAL_DEFAULT_PROPERTIES += \
+    dalvik.vm.dex2oat-filter=speed \
     ro.sys.fw.dex2oat_thread_count=4
 
 # Audio
